fe1689fbf209cbdb37c956f8c144064f96dffded
Wang Zaixi <wangzaixi01@gmail.com>
2026-01-28 23:08:17 +0800

    Implement simple chatbot command. Echo user input and exit on 'bye'

--- src/main/java/Algo.java
+++ src/main/java/Algo.java
@@ -1,27 +1,41 @@
+import java.util.Scanner;
+
 public class Algo {
     static final String BOT_NAME = "ALGO";
     static final String LINE = "________________________________________________________";
     static final String LOGO =
-            "     ___    _      ____   ___  \n" +
-                    "    / _ \\  | |    / ___| / _ \\ \n" +
-                    "   / /_\\ \\ | |   | |  _ | | | |\n" +
-                    "   |  _  | | |___| |_| || |_| |\n" +
-                    "   |_| |_| |_____\\____/  \\___/ \n";
+            "  ___    _      ____   ___  \n" +
+                    " / _ \\  | |    / ___| / _ \\ \n" +
+                    "/ /_\\ \\ | |   | |  _ | | | |\n" +
+                    "|  _  | | |___| |_| || |_| |\n" +
+                    "|_| |_| |_____\\____/  \\___/\n";
 
-    public static void print_simple_greeting() {
-        System.out.println(LINE);
-        System.out.println(LOGO);
-        System.out.println(" Hello! I'm " + BOT_NAME);
-        System.out.println(" What can I do for you?");
-        System.out.println(LINE);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(LINE);
-
-    }
 
     public static void main(String[] args) {
-        print_simple_greeting();
+        Scanner in = new Scanner(System.in);
+
+        printGreeting();
+        String input = in.nextLine();
+
+        while(!input.equalsIgnoreCase("bye")) {
+            printDialogueBox(input);
+            input = in.nextLine();
+        }
+
+        printDialogueBox("Bye. Hope to see you again soon!");
     }
 
-
+    private static void printGreeting() {
+        System.out.println(LINE);
+        System.out.println(LOGO);
+        System.out.println("Hello! I'm "+ BOT_NAME + "\nWhat can I do for you?");
+        System.out.println(LINE);
+        System.out.println();
+    }
+    private static void printDialogueBox(String text) {
+        System.out.println(LINE);
+        System.out.println(text);
+        System.out.println(LINE);
+        System.out.println();
+    }
 }
