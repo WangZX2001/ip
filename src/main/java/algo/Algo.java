6f5ea903b225cc5aa939233e1956641dc29170fc
Wang Zaixi <wangzaixi01@gmail.com>
2026-02-23 00:48:45 +0800

    Change the structure of the algo main class to directly run the program

--- src/main/java/algo/Algo.java
+++ src/main/java/algo/Algo.java
@@ -1,94 +1,46 @@
 package algo;
 
-import algo.task.Task;
-import java.util.List;
-
 public class Algo {
 
-    private static TaskList tasks;
-    private static final String FILE_PATH = "data/algo.txt";
-    private static final Storage storage = new Storage(FILE_PATH);
+    private final Storage storage;
+    private TaskList tasks;
+    private final Ui ui;
 
-
-    public static void main(String[] args) {
-        Ui ui = new Ui();
+    public Algo(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
         try {
-            List<Task> loaded = storage.load();
-            tasks = new TaskList(loaded);
+            tasks = new TaskList(storage.load());
         } catch (AlgoException e) {
             ui.showLoadingError();
             tasks = new TaskList();
         }
-        ui.showWelcomeMessage();
-        executeCommand(ui);
+    }
+
+    public void run() {
+        ui.showWelcome();
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                ui.printLine();
+                Command c = Parser.parse(fullCommand);
+                c.execute(tasks, ui, storage);
+                isExit = c.isExit();
+            } catch (AlgoException e) {
+                ui.showError(e.getMessage());
+            } finally {
+                if (!isExit) {
+                    ui.printLine();
+                }
+            }
+        }
         ui.showByeMessage();
         ui.close();
     }
 
-    private static void executeCommand(Ui ui) {
-        while (true) {
-            String input = ui.readCommand();
-
-            try {
-                ParsedCommand parsed = Parser.parseCommand(input);
-
-                switch (parsed.command) {
-
-                case "bye":
-                    return;
-
-                case "list":
-                    ui.printLine();
-                    System.out.println(tasks.listTasks());
-                    ui.printLine();
-                    break;
-
-                case "todo":
-                case "deadline":
-                case "event": {
-                    String msg = tasks.addTask(parsed.fullInput);
-                    storage.save(tasks.getAll());
-                    ui.printLine();
-                    System.out.println(msg);
-                    ui.printLine();
-                    break;
-                }
-
-                case "mark": {
-                    String msg = tasks.setDone(parsed.args, true);
-                    storage.save(tasks.getAll());
-                    ui.printLine();
-                    System.out.println(msg);
-                    ui.printLine();
-                    break;
-                }
-
-                case "unmark": {
-                    String msg = tasks.setDone(parsed.args, false);
-                    storage.save(tasks.getAll());
-                    ui.printLine();
-                    System.out.println(msg);
-                    ui.printLine();
-                    break;
-                }
-
-                case "delete": {
-                    String msg = tasks.deleteTask(parsed.args);
-                    storage.save(tasks.getAll());
-                    ui.printLine();
-                    System.out.println(msg);
-                    ui.printLine();
-                    break;
-                }
-
-                default:
-                    throw new AlgoException("Invalid command.");
-                }
-
-            } catch (AlgoException e) {
-                ui.showErrorMessage(e.getMessage());
-            }
-        }
+    public static void main(String[] args) {
+        new Algo("data/algo.txt").run();
     }
 }
 
